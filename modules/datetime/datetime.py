8803f73d71d897f98b9511f990c48769975abff4
Selleslagh Tom <Tom@iMac-de-Selleslagh.local>
2016-06-24 11:50:07 +0200

    Add page link to a module

--- modules/datetime/datetime.py
+++ modules/datetime/datetime.py
@@ -10,11 +10,13 @@ class DateTime(Module):
     '''Class representing a module that shows current date and time.'''
     def __init__(self):
         super().__init__('Date and Time')
-    
+
     def widget(self):
         def render():
             return template('./modules/datetime/widget.tpl')
         return render
-    
+
     def page(self):
-        return None
+        def render():
+            return template('./modules/datetime/widget.tpl')
+        return render
